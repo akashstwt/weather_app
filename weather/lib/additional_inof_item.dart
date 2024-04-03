2f857ed2299bb30a094205bf606365336a8c52c7
akashstwt <akashpatel115347@gmail.com>
2024-04-03 18:46:23 +0530

    initial commit

---
+++ weather/lib/additional_inof_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AdditionalInformation extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final String value;
+  const AdditionalInformation({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.value,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 8),
+        Icon(
+          icon,
+          size: 32,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          label,
+          style: const TextStyle(
+              //fontSize: 16,
+              ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ weather/lib/secrets.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = 'da0346a2014fcd6fdff4938f9ae0080a';
